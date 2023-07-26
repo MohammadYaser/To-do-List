6d7029d2755e79bc91780bc322e556e20ce3871d
MohammadYaser <yasersafi19@outlook.com>
2023-07-26 10:19:05 +0430

    Use html template with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: "development",
     entry: {
@@ -8,5 +9,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].js",
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'To Do List',
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+    ],
 
 }
