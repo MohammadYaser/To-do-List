e99cadea44b94688b80ca517ac72e57d95fe12c2
MohammadYaser <yasersafi19@outlook.com>
2023-07-27 00:39:53 +0430

    Configure linter

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import './style.css'
+import './style.css';
 
 const listTasks = [
-    {
-      description: 'Going to Office',
-      completed: false,
-      index: 0,
-    },
-    {
-      description: 'Meeting with ali',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Go to market',
-      completed: false,
-      index: 2,
-    },
-    
-  ];
-  const listCard = document.getElementById('list');
-  listTasks.forEach((arr) => {
-    const task = `
+  {
+    description: 'Going to Office',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Meeting with ali',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go to market',
+    completed: false,
+    index: 2,
+  },
+
+];
+const listCard = document.getElementById('list');
+listTasks.forEach((arr) => {
+  const task = `
     <div class="card-task">
         <input type="checkbox">
         <div class="card list">${arr.description}</div>
@@ -28,4 +28,4 @@ const listTasks = [
     <div>
   `;
   listCard.insertAdjacentHTML('beforeend', task);
-  });
+});
